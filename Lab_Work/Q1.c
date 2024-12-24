bb7a08e59ed90a5ac3e5656fde5dad3439cf7e2c
chandaniptl <chandanip910@gmail.com>
2024-12-24 11:39:53 +0530

    Delete Lab_Work directory

--- Lab_Work/Q1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>;
-#include<conio.h>;
-int main(){
-    printf("My Name Is Chandani Patel\n");
-    printf("My Age is 21\n");
-    printf("My School Name Is Kanya Vidhyalaya Astan\n");
-    return 0;
-}
